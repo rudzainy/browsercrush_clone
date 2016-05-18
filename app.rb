ccdc615fb1137f982061fd4f269a6dbe20373466
pynkvalerian <yenping.chew90@gmail.com>
2016-05-18 23:03:45 +0800

    initial app

--- app.rb
+++ app.rb
@@ -1,18 +1,6 @@
 require 'sinatra'
 require 'erb'
 
-# This gives you the homepage of your app.
-get '/calculator' do
-  erb :calculator
-end
-
-# This handles the data from your calculator form.
-post '/calculator' do
-  math_operator = params[:math_operator]           # eg: +, -, *, /
-  @first_number = params[:first_num].to_i   # eg: 5
-  @second_number = params[:second_num].to_i # eg: 6
-
-  # Start coding here, friends!
-
-  erb :calculator
+get '/' do
+  erb :crush
 end
---
+++ public/application.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#start').on('click', function(){
+    alert('ready!')
+  })
+})
